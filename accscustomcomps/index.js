c753ef7cc39b7b9e16a79196900b05df603092e4
Steven Davelaar <steven.davelaar@oracle.com>
2019-06-21 11:11:47 +0200

    Cleaned up and processed review comments for making this reusable asset

--- accscustomcomps/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var Components = require('./components.js');
-
-// Create a server instance
-var server = Components('/components');
-
-// Start the server listening..
-server.listen(process.env.PORT || 7950);
