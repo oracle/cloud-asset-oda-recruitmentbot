7e1a30e79026eb63fbb012130911ac2708d90668
Steven Davelaar <steven.davelaar@oracle.com>
2019-09-16 11:34:00 +0200

    changes requested by legal

--- customcomponents/components/findJobs.js
+++ customcomponents/components/findJobs.js
@@ -1,6 +1,7 @@
 /*******************************************************************************
- Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
-******************************************************************************/
+ Copyright © 2019, Oracle and/or its affiliates. All rights reserved.
+ Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+ ******************************************************************************/
 "use strict";
 const jobInfo = require('./jobInfo.json');
 const images = require('./images.json');
